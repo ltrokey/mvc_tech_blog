17ff17dda6838667452dc233c01aa1028a38fae6
Lacey Trokey <trokeyln@gmail.com>
2023-11-21 12:37:40 -0700

    Fixed errors with server

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 const dashboardRoutes = require("./dashboardRoutes");
-const { errorHandler, notFoundHandler } = require("../../utils/helpers");
+const { errorHandler, notFoundHandler } = require("../utils/helpers");
 
 router.use("/api", apiRoutes);
 router.use("/home", homeRoutes);
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,10 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-};
-
-const notFoundHandler = (req, res) => {
-  res.status(404).send("Not Found");
-};
-
 module.exports = {
-  errorHandler,
-  notFoundHandler,
+  errorHandler: (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+  },
+
+  notFoundHandler: (req, res) => {
+    res.status(404).send("Not Found");
+  },
 };
